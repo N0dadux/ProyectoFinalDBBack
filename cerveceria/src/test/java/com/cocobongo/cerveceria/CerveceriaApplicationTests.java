93301ddd48c2c4fc6ce80688ec5d133cd5473b95
N0dadux <juddiazp@udistrital.edu.co>
2026-04-12 17:55:37 -0500

    First commit, Add: Spring Boot Initializr

---
+++ cerveceria/src/test/java/com/cocobongo/cerveceria/CerveceriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocobongo.cerveceria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerveceriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
