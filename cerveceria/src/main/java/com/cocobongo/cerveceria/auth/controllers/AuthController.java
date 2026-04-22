20d13ebfc985966aef7a37fbacb8cac07c07a162
N0dadux <juddiazp@udistrital.edu.co>
2026-04-22 10:23:05 -0500

    Add: module Auth and in the module Users the UserEntity

---
+++ cerveceria/src/main/java/com/cocobongo/cerveceria/auth/controllers/AuthController.java
@@ -0,0 +1,5 @@
+package com.cocobongo.cerveceria.auth.controllers;
+
+public class AuthController {
+
+}
---
+++ cerveceria/src/main/java/com/cocobongo/cerveceria/auth/entities/AuditEntity.java
@@ -0,0 +1,5 @@
+package com.cocobongo.cerveceria.auth.entities;
+
+public class AuditEntity {
+
+}
