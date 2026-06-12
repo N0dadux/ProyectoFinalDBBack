60bf7f8d43139ca553d25662483be276e809de30
Carlos E <sangroniscarlosenrique1@gmail.com>
2026-06-12 14:32:22 -0500

    Add: EnableScheduling en el Application para permitir el @Scheduled

--- cerveceria/src/main/java/com/cocobongo/cerveceria/CerveceriaApplication.java
+++ cerveceria/src/main/java/com/cocobongo/cerveceria/CerveceriaApplication.java
@@ -3,9 +3,11 @@ package com.cocobongo.cerveceria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync   // Habilita el @Async en AuditService
+@EnableScheduling  // Habilita el @Scheduled en AuditService
 public class CerveceriaApplication {
 
 	public static void main(String[] args) {
